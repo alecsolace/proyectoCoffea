eb4f1f36fcaa9d6863e8312aa2fc7517e9bf705d
Alexander Aguirre <keevinaguirre@gmail.com>
2022-05-10 20:15:39 +0200

    Migración de front a la plantilla

---
+++ netflix/src/main/java/com/sanvalero/netflix/dao/CustomerDAO.java
@@ -0,0 +1,116 @@
+
+package com.sanvalero.netflix.dao;
+
+import com.sanvalero.netflix.domain.Customer;
+import com.sanvalero.netflix.domain.Movie;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class CustomerDAO {
+
+    private static final String DRIVER = "com.oracle.database.jdbc";
+    private static final String URL_CONEXION = "jdbc:oracle:thin:@localhost:1521/XE";
+    private static final String USUARIO = "ADMIN";
+    private static final String CONTRASENA = "ADMIN";
+    private ArrayList<Customer> customers;
+
+    private Connection connection;
+
+    public CustomerDAO() throws SQLException {
+        connect();
+        customers = getAllCustomers();
+    }
+
+    /**
+     * Conecta con la base de datos
+     */
+    public void connect() {
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL_CONEXION, USUARIO, CONTRASENA);
+        } catch (ClassNotFoundException cnfe) {
+            cnfe.printStackTrace();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
+
+    /**
+     * Desconecta de la base de datos
+     */
+    public void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
+
+    /**
+     * Adds a customer to the database
+     * 
+     * @param movie The customer with the information you want to add
+     * @throws SQLException
+     */
+    public void addUser(Customer customer) throws SQLException {
+
+        String sql = "INSERT INTO CUSTOMERS (CUSTOMER_ID, ADDRESS_ID, NAME, LAST_NAME, EMAIL, PASSWORD) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement sentencia = connection.prepareStatement(sql);
+        sentencia.setInt(1, customer.getUserID());
+        sentencia.setInt(2, customer.getAddress_ID());
+        sentencia.setString(3, customer.getName());
+        sentencia.setString(4, customer.getLastName());
+        sentencia.setString(5, customer.getEmail());
+        sentencia.setString(6, customer.getPassword());
+        sentencia.executeUpdate();
+    }
+
+    /**
+     * Obtains an ArrayList with all the customers present in the DDBB
+     * 
+     * @return An ArrayList of Customers
+     */
+    public ArrayList<Customer> getAllCustomers() throws SQLException {
+
+        String query = "SELECT * FROM CUSTOMERS";
+        Statement statement = connection.createStatement();
+        ResultSet results = statement.executeQuery(query);
+        ArrayList<Customer> customerList = new ArrayList<>();
+
+        while (results.next()) {
+            Customer customer = new Customer(results.getInt("ADDRESS_ID"), results.getString("NAME"),
+                    results.getString("LAST_NAME"), results.getString("EMAIL"), results.getString("PASSWORD"));
+            customerList.add(customer);
+        }
+        return customerList;
+    }
+
+    /**
+     * Elimina una película
+     * 
+     * @param id El id de la pelicula a eliminar
+     */
+    public void removeCustomer(int id) {
+        for (Customer customer : customers) {
+            if (customer.getUserID() == id) {
+                customers.remove(customer);
+            }
+        }
+    }
+
+    /**
+     * Modifica la información de una pelicula
+     * 
+     * @param movie La película con la información a modificar
+     */
+    public void modifyMovie(Movie movie) {
+
+    }
+}
---
+++ netflix/src/main/java/com/sanvalero/netflix/domain/Customer.java
@@ -0,0 +1,102 @@
+package com.sanvalero.netflix.domain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class Customer {
+    private int userID = 0;
+    private int address_ID;
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public Customer(int address, String name, String lastName, String email, String password) {
+        this.address_ID = address;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        registrarUsuario();
+        userID++;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAddress_ID() {
+        return address_ID;
+    }
+
+    public void setAddress_ID(int address_ID) {
+        this.address_ID = address_ID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void registrarUsuario() {
+        // Insertar los metodos de la base de datos
+        int filas = 0;
+        try {
+            Connection connection;
+            String cadenaConexion = "jdbc:oracle:thin:@localhost:1521/XE";
+            connection = DriverManager.getConnection(cadenaConexion, "ADMIN", "ADMIN");
+
+            String createUser = "INSERT INTO CUSTOMERS VALUES(?,?,?,?,?,?)";
+            PreparedStatement userStatement = connection.prepareStatement(createUser);
+
+            userStatement.setInt(1, userID);
+            userStatement.setInt(2, this.address_ID);
+            userStatement.setString(3, this.name);
+            userStatement.setString(4, this.lastName);
+            userStatement.setString(5, this.email);
+            userStatement.setString(6, this.password);
+
+            filas = userStatement.executeUpdate();
+            System.out.println("Se han insertado: " + filas + " filas.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void modificarUsuario() {
+        // Insertar la query de modificación.
+    }
+}
